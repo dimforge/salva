b98b3fd81a6be90091915a21e1ba70981d75a6e2
Thierry Berger <contact@thierryberger.com>
2024-08-26 14:02:09 +0200

    update bevy 0.14 + fix some warnings

--- examples2d/all_examples2.rs
+++ examples2d/all_examples2.rs
@@ -23,7 +23,7 @@ fn demo_name_from_command_line() -> Option<String> {
     None
 }
 
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(target_arch = "wasm32"))]
 fn demo_name_from_url() -> Option<String> {
     let window = stdweb::web::window();
     let hash = window.location()?.search().ok()?;
@@ -34,7 +34,7 @@ fn demo_name_from_url() -> Option<String> {
     }
 }
 
-#[cfg(not(any(target_arch = "wasm32", target_arch = "asmjs")))]
+#[cfg(not(any(target_arch = "wasm32")))]
 fn demo_name_from_url() -> Option<String> {
     None
 }
